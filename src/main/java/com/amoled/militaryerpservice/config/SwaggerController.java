5763d8067b784ab35887817ce274a4b4a337172c
DELL_DEV_BOOK <byn_purple@naver.com>
2021-01-08 18:17:16 +0900

    Initial commit

---
+++ src/main/java/com/amoled/militaryerpservice/config/SwaggerController.java
@@ -0,0 +1,17 @@
+package com.amoled.militaryerpservice.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerController {
+//    @GetMapping("/docs/ui")
+//    fun ui(): String {
+//        return "redirect:/swagger-ui.html"
+//    }
+//
+//    @GetMapping("/docs/json")
+//    fun json(): String {
+//        return "redirect:/v2/api-docs"
+//    }
+}
---
+++ src/test/java/com/amoled/militaryerpservice/MilitaryErpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amoled.militaryerpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryErpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
