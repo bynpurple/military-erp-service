35bf09aabea2fce3dff708831654db418a8f3b0f
DELL_DEV_BOOK <byn_purple@naver.com>
2021-01-10 23:33:12 +0900

    BoardController

---
+++ src/main/java/com/amoled/militaryerpservice/controller/board/BoardController.java
@@ -0,0 +1,10 @@
+package com.amoled.militaryerpservice.controller.board;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/board")
+public class BoardController {
+}
